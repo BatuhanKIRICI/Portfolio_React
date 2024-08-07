d8bf2f50d1cfcafb2a4b60566f0a8cbf542b0925
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-08-07 11:59:08 +0200

    "List components"

---
+++ src/data.js
@@ -0,0 +1,39 @@
+import Img1 from "./img/address.png";
+import Img2 from "./img/award.png";
+import Img3 from "./img/email.png";
+import Img4 from "./img/phone.png";
+import Img5 from "./img/sun.png";
+import Img6 from "./img/pexels-goumbik-574071.jpg";
+
+export const products = [
+  {
+    id: 1,
+    img: Img1,
+    link: "https://github.com/BatuhanKIRICI",
+  },
+  {
+    id: 2,
+    img: Img2,
+    link: "https://github.com/BatuhanKIRICI",
+  },
+  {
+    id: 3,
+    img: Img3,
+    link: "https://github.com/BatuhanKIRICI",
+  },
+  {
+    id: 4,
+    img: Img4,
+    link: "https://github.com/BatuhanKIRICI",
+  },
+  {
+    id: 5,
+    img: Img5,
+    link: "https://github.com/BatuhanKIRICI",
+  },
+  {
+    id: 6,
+    img: Img6,
+    link: "https://github.com/BatuhanKIRICI",
+  },
+];
