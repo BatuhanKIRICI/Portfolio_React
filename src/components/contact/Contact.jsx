67eaddbdb219eb4209b94ee783a3b2cef6466648
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-08-09 23:17:40 +0200

    "Styling form"

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,15 @@ import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
+import { useRef } from "react";
 
 const Contact = () => {
+  const formRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="c">
       <div className="c-bg"></div>
@@ -17,7 +24,7 @@ const Contact = () => {
             </div>
             <div className="c-info-item">
               <img src={Email} alt="" className="c-icon" />
-              +49 176 564235
+              batuhankirici01@gmail.com
             </div>
             <div className="c-info-item">
               <img src={Address} alt="" className="c-icon" />
@@ -25,7 +32,22 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <div className="c-right"></div>
+        <div className="c-right">
+          <p className="c-desc">
+            My proficiency in programming languages such as HTML, CSS,
+            JavaScript, and TypeScript, as well as CSS and frameworks such as
+            React and Next.js, allow me to build dynamic and scalable
+            applications. In addition, I have experience in version control with
+            GitHub methodologie.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows={5} placeholder="Message" name="message" />
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
